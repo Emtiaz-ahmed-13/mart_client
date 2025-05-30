f9f4bb35fb9fe71c57f188db12a94b11e23a6a54
Emtiaz-ahmed-13 <emtiaz.ahmed@g.bracu.ac.bd>
2025-05-30 19:06:10 +0600

    feat:complete-part2

---
+++ src/app/(withcommonLayout)/create-shop/page.tsx
@@ -0,0 +1,11 @@
+import CreateShopForm from "@/components/modules/auth/shop/Create-shop/CreateShopForm";
+
+const CreateShopPage = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <CreateShopForm />
+    </div>
+  );
+};
+
+export default CreateShopPage;
